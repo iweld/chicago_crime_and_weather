ad01deb22f7939ed46f87eb872bf1989279436bd
Jaime M. Shaker <eyeweld@gmail.com>
2022-11-15 18:48:34 -0600

    Adding navigation link and altering csv path links.

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 A simple analysis of taking Chicago crime and average daily temperature data for 2021.
 
 ## Datasets used
-Three key datasets for this case study
+Three key [datasets](https://github.com/iweld/chicago_crime_and_weather_2021/tree/main/csv) for this case study
 - <strong>chicago_areas.csv</strong>: Chicago neighborhoods and areas.
 - <strong>chicago_crimes_2021.csv</strong>: Reported crimes in 2021.
 - <strong>chicago_temps_2021.csv</strong>: Average city temperatures.
 
 ## Entity Relationship Diagram
 ![alt text](https://github.com/iweld/chicago_crime_and_weather_2021/blob/main/ERD.JPG)
+
--- sql_schema.sql
+++ sql_schema.sql
@@ -44,7 +44,7 @@ COPY CRIMES (
 	COMMUNITY_ID,
 	LATITUDE,
 	LONGITUDE)
-FROM 'C:\Users\Jaime\Desktop\git-repo\chicago_crime_and_weather_2021\csv\chicago_crimes_2021.csv'
+FROM 'path/to/chicago_crimes_2021.csv'
 DELIMITER ',' CSV HEADER;
 
 COPY COMMUNITY (
@@ -53,7 +53,7 @@ COPY COMMUNITY (
 	POPULATION, 
 	AREA_SIZE, 
 	DENSITY)
-FROM 'C:\Users\Jaime\Desktop\git-repo\chicago_crime_and_weather_2021\csv\chicago_areas.csv'
+FROM 'path/to/chicago_areas.csv'
 DELIMITER ',' CSV HEADER;
 
 COPY WEATHER (
@@ -61,5 +61,5 @@ COPY WEATHER (
 	TEMP_HIGH, 
 	TEMP_LOW, 
 	PRECIPITATION)
-FROM 'C:\Users\Jaime\Desktop\git-repo\chicago_crime_and_weather_2021\csv\chicago_temps_2021.csv'
+FROM 'path/to/chicago_temps_2021.csv'
 DELIMITER ',' CSV HEADER;
